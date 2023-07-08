776fe269e917e3b7e75c4e3efd0b49138f213857
VazgenBablumyan <vazgeb@gmail.com>
2023-07-08 18:39:24 +0400

    feat(SelectCountry.jsx): fix bug

--- src/Components/Select/SelectCountry.jsx
+++ src/Components/Select/SelectCountry.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React  from 'react'
 import { countries } from 'country-data-list';
 import styles from './SelectCountry.module.css'
 import { v4 as uuidv4 } from 'uuid';
@@ -7,12 +7,11 @@ import ListItemText from '@mui/material/ListItemText';
 import { ListItemButton } from '@mui/material';
 
 export default function SelectCountry({ name }) {
-    const countriesName = []
-    for (const country in countries) {
-        countriesName.push(countries[country].name)
-    }
+    
+    const countriesName = countries.all.map((country)=> country.name)
+    console.log(countriesName)
 
-    const filteredNames = countriesName.filter((countryName,idx)=> idx % 2 === 1).filter((countryName) => {
+    const filteredNames = countriesName.sort().filter((countryName) => {
        const lowercaseCountryNames = countryName.toLowerCase()
        const lowercaseName = name.toLowerCase()
         for (let i = 0; i < name.length; i++) {
@@ -20,13 +19,14 @@ export default function SelectCountry({ name }) {
                 return false;
             }
         }
+        
         return true;
     })
     
          return (
             <List className={styles.listBox}>
 
-                {filteredNames.sort().map((countryname) => {
+                {filteredNames.map((countryname) => {
                 const id = uuidv4()
                 return (
                 <ListItemButton key ={id}> <ListItemText>{countryname}</ListItemText></ListItemButton> 
